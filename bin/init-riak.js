b4782fd77718cdd5bc544c38a02d16c6cf72f9fe
jane arc <jane@cpan.org>
2015-03-17 13:54:21 -0400

    rm nopt for minimist rm the plusconcats add halpfulness update package.json

--- bin/init-riak.js
+++ bin/init-riak.js
@@ -1,42 +1,44 @@
 #!/usr/bin/env node
 
-var nopt = require('nopt')
-	, knownopts = {
-			'dry-run'         : [ Boolean ],
-			'reap-empties'    : [ Boolean ],
-			'verbose'         : [ Boolean ]
-		}
-	, parsed = nopt( knownopts, process.argv )
+var args = require( 'minimist' )(process.argv.slice(2));
+var riak = require( 'riak-dc' );
 
-	, Riak = require( 'riak-dc' )
+if (args.halp || args.help || args.h) {
+	[ 'Usage:',
+		'--dry-run      means don\'t actually burn the db',
+		'--reap-empties means delete empty tuples',
+		'--verbose      means tell me everything' ]
+			.forEach( function (h) { console.log(h) } )
+	process.exit(0);
+}
 
 function default_logger (s) {
-	if (process.env['DEBUG'] || parsed['verbose']) {
+	if (process.env['DEBUG'] || args['verbose']) {
 		console.log(s);
 	}
 }
 
-Riak.get_buckets().then( function (buckets) {
+riak.get_buckets().then( function (buckets) {
 	buckets.forEach( function (bucket) {
-		default_logger( 'traversing bucket: ' + bucket );
-		Riak.get_keys( bucket ).then( function (keys) {
+		default_logger( 'traversing bucket: '.concat( bucket ) );
+		riak.get_keys( bucket ).then( function (keys) {
 			keys.forEach( function (key) {
-				default_logger( 'retrieving key: ' + key );
-				Riak.get_tuple( bucket, key ).then( function ( tuple ) {
-					var tid = bucket + '/' + key;
+				default_logger( 'retrieving key: '.concat( key ) );
+				riak.get_tuple( bucket, key ).then( function ( tuple ) {
+					var tid = bucket.concat( '/', key );
 					if (typeof tuple == 'error') {
-						default_logger( 'tuple ' + tid + ' found empty' );
-						if (!parsed['dry-run'] && parsed['reap-empties']) {
-							default_logger( 'reaping empty tuple ' + tid );
+						default_logger( 'tuple '.concat(tid, ' found empty' ) );
+						if (!args['dry-run'] && args['reap-empties']) {
+							default_logger( 'reaping empty tuple '.concat( tid ) );
 						}
 					}
-					if (!parsed['dry-run']) {
-						default_logger( 'deleting extant tuple ' + tid );
-						Riak.del_tuple( bucket, key ).then( function () {
-							default_logger( 'tuple ' + tid + ' deleted.' );
+					if (!args['dry-run']) {
+						default_logger( 'deleting extant tuple '.concat( tid ) );
+						riak.del_tuple( bucket, key ).then( function () {
+							default_logger( 'tuple '.concat( tid, ' deleted.' ) );
 						} );
 					}
-					default_logger( bucket + '/' + key + ': ' + tuple )
+					default_logger( bucket.concat( '/', key, ': ', tuple ) );
 				} ) // get_tuple
 			} ) // keys.foreach
 		} ) // get_keys
